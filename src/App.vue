66642becc1eb09665075be81f91d57a22c9a70c1
OwaisRaza <owaisraza1989@gmail.com>
2021-06-29 19:12:32 +0500

    create vue app setup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,78 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-menu-item key="1">
+          <a-icon type="user" />
+          <span>Home</span>
+            <router-link to="/"></router-link>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="video-camera" />
+          <span>About</span>
+            <router-link to="/about"></router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+      >
     <router-view/>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+#components-layout-demo-custom-trigger {
+  height: 100vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+.logo {
+    width: 100%;
+    height: 55px;
+    background: url(/img/logo.82b9c7a5.png) no-repeat center;
+    background-size: 22%;
 }
 </style>
